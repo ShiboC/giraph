613689959071409bc8dd9d111ba8fec7bfa59bc4
root <root@gp15.fak6.win.tu-berlin.de>
2018-03-26 10:40:29 +0200

    eventlooptime60s tasktimeout60s

--- giraph-core/src/main/java/org/apache/giraph/master/BspServiceMaster.java
+++ giraph-core/src/main/java/org/apache/giraph/master/BspServiceMaster.java
@@ -1301,10 +1301,10 @@ public class BspServiceMaster<I extends WritableComparable,
     List<String> finishedHostnameIdList = new ArrayList<>();
     long nextInfoMillis = System.currentTimeMillis();
     final int defaultTaskTimeoutMsec = 4 * 60 * 1000;  // from TaskTracker. previous 10.
-    final int waitBetweenLogInfoMsec = 15 * 1000; //previous 30
+    final int waitBetweenLogInfoMsec = 60 * 1000; //previous 30
 //    final int taskTimeoutMsec = getContext().getConfiguration().getInt(
 //        "mapred.task.timeout", defaultTaskTimeoutMsec) / 2;
-    final int taskTimeoutMsec=2*60*1000;
+    final int taskTimeoutMsec=1*60*1000;
     long lastRegularRunTimeMsec = 0;
     int eventLoopTimeout =  Math.min(taskTimeoutMsec, waitBetweenLogInfoMsec);
     boolean logInfoOnlyRun = false;
