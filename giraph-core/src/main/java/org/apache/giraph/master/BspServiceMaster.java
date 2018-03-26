46964e5432750c44219a97f1e446cf3da8780fa4
root <root@gp15.fak6.win.tu-berlin.de>
2018-03-26 11:35:02 +0200

    change to default

--- giraph-core/src/main/java/org/apache/giraph/master/BspServiceMaster.java
+++ giraph-core/src/main/java/org/apache/giraph/master/BspServiceMaster.java
@@ -1300,11 +1300,11 @@ public class BspServiceMaster<I extends WritableComparable,
         getWorkerInfoHealthyPath(getApplicationAttempt(), getSuperstep());
     List<String> finishedHostnameIdList = new ArrayList<>();
     long nextInfoMillis = System.currentTimeMillis();
-    final int defaultTaskTimeoutMsec = 4 * 60 * 1000;  // from TaskTracker. previous 10.
-    final int waitBetweenLogInfoMsec = 60 * 1000; //previous 30
-//    final int taskTimeoutMsec = getContext().getConfiguration().getInt(
-//        "mapred.task.timeout", defaultTaskTimeoutMsec) / 2;
-    final int taskTimeoutMsec=1*60*1000;
+    final int defaultTaskTimeoutMsec = 10 * 60 * 1000;  // from TaskTracker. previous 10.
+    final int waitBetweenLogInfoMsec = 30 * 1000; //previous 30
+    final int taskTimeoutMsec = getContext().getConfiguration().getInt(
+        "mapred.task.timeout", defaultTaskTimeoutMsec) / 2;
+//    final int taskTimeoutMsec=1*60*1000;
     long lastRegularRunTimeMsec = 0;
     int eventLoopTimeout =  Math.min(taskTimeoutMsec, waitBetweenLogInfoMsec);
     boolean logInfoOnlyRun = false;
